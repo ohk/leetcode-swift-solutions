46eac511fc622b85263b171ae2545b33d1111d5e
Omer Hamid Kamisli <me@ohkamisli.dev>
2022-11-02 23:25:19 -0700

    104 - Maximum Depth of Binary Tree solution added.

---
+++ 104-Maximum-Depth-of-Binary-Tree.swift
@@ -0,0 +1,51 @@
+/**
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    
+    func maxDepthRecursive(_ root:TreeNode?,_ level: Int) -> Int {
+        guard let root = root else { return level}
+        return max(maxDepthRecursive(root.left, level + 1),maxDepthRecursive(root.right, level + 1))
+    }
+
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        return maxDepthRecursive(root,0)
+    }
+}
